5aa5aa564113b06ff241e0a54333b5e8db3be08e
testerchu0 <testerchu0@gmail.com>
2019-10-01 20:09:21 +0800

    init

---
+++ precache-manifest.1959f149bd25501b8532c85bcd085c88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/popovan/robots.txt"
+  },
+  {
+    "revision": "3f5f4c3d286b617c1b5c",
+    "url": "/popovan/js/chunk-vendors.d165edf1.js"
+  },
+  {
+    "revision": "ccc1572766c99470ce5e",
+    "url": "/popovan/js/app.f83b1ff5.js"
+  },
+  {
+    "revision": "1981919490bfe0cb4d63986813b4e13a",
+    "url": "/popovan/index.html"
+  },
+  {
+    "revision": "ccc1572766c99470ce5e",
+    "url": "/popovan/css/app.3c27f8dd.css"
+  }
+];
